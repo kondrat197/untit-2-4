6dfd6bbe5a422496f72636a34a7e69012795e104
Stud2 <kondrat197@list.ru>
2019-11-23 16:36:16 +0300

    ghjhjbgbbj

--- src/ru/kondrat/unicorn/Application.java
+++ src/ru/kondrat/unicorn/Application.java
@@ -3,7 +3,7 @@ package ru.kondrat.unicorn;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.print("Привет!");
+        System.out.print("Привет. world!");
     }
 
 }
